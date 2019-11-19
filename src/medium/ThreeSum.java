a66681b8803b00e39a4913896c2b3e4a3cc0263f
acrush37 <acrush37@gmail.com>
2019-11-19 22:11:53 +0800

    LeetCode Array

--- src/medium/ThreeSum.java
+++ src/medium/ThreeSum.java
@@ -2,6 +2,8 @@ package medium;
 
 import java.util.*;
 
+import static java.util.Map.Entry;
+
 /*
     Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0?
 
@@ -11,48 +13,65 @@ public class ThreeSum {
 
     public static void main(String... args) {
 
-        int[] nums = {0, 0, 0, 0};
+        int[] nums = {-6,7,-6,-1,1,-3,-6,7,5,-4,-3,5};
         ThreeSum threeSum = new ThreeSum();
         System.out.println(threeSum.threeSum(nums));
     }
 
     public List<List<Integer>> threeSum(int[] nums) {
 
+        int s = 0;
+        boolean zero = false;
         int n = nums.length;
-        Arrays.sort(nums);
-        Set<Integer> t = new HashSet<>();
-        Set<String> set = new HashSet<>();
-        Map<Integer, Set<Integer[]>> m = new HashMap<>();
+        int[] f = new int[n];
+        Map<Integer, Integer> m = new HashMap<>();
         List<List<Integer>> result = new ArrayList<>();
 
-        for (int i = 1; i < n-1; i++)
-            for (int j = i+1; j < n; j++) {
+        for (int x : nums) {
 
-                String s = nums[i] + "," + nums[j];
-                if (set.contains(s)) continue;
-                set.add(s);
-                int key = -nums[i]-nums[j];
-                set.add(nums[j] + "," + nums[i]);
-                set.add(nums[j] + "," + key);
-                set.add(key + "," + nums[j]);
-                set.add(key + "," + nums[i]);
-                set.add(nums[i] + "," + key);
-                Set<Integer[]> value = m.get(key);
-                if (value == null) value = new HashSet<>();
-                value.add(new Integer[]{nums[i], nums[j]});
-                m.put(key, value);
+            Integer y = m.get(x);
+            m.put(x, y == null ? 1 : 1 + y);
+        }
+
+        if (m.containsKey(0)) {
+
+            zero = true;
+            if (m.get(0) >= 3) result.add(Arrays.asList(0, 0, 0));
+        }
+
+        Set<Entry<Integer, Integer>> entries = m.entrySet();
+
+        for (Entry<Integer, Integer> entry : entries) {
+
+            int key = entry.getKey();
+
+            if (key != 0) {
+
+                f[s++] = key;
+                if (key > 0 && zero && m.containsKey(-key)) result.add(Arrays.asList(0, key, -key));
+
+                if (entry.getValue() >= 2) {
+                    int x = -(key << 1);
+                    if (m.containsKey(x)) result.add(Arrays.asList(key, key, x));
+                }
             }
+        }
 
-        for (int i = 0; i < n; i++)
-            if (!t.contains(nums[i])) {
+        Arrays.sort(f, 0, s);
+        m = new HashMap<>();
+        for (int i = 0; i < s; i++) m.put(f[i], i);
+        boolean[][] t = new boolean[s][s];
 
-                t.add(nums[i]);
-                Set<Integer[]> s = m.get(nums[i]);
+        for (int i = 0; i < s-2; i++)
+            for (int j = i+1; j < s-1; j++)
+                if (!t[i][j]) {
 
-                if (s != null)
-                    for (Integer[] f : s)
-                        result.add(Arrays.asList(nums[i], f[0], f[1]));
-            }
+                    int k = -f[i] - f[j];
+                    Integer x = m.get(k);
+                    if (x == null || x == i || x == j) continue;
+                    t[i][x] = t[x][i] = t[x][j] = t[j][x] = true;
+                    result.add(Arrays.asList(f[i], f[j], k));
+                }
 
         return result;
     }
