18f3c52c8a3f4561eeb7854b8735aaf9c7744c95
acrush37 <acrush37@gmail.com>
2019-12-31 22:17:02 +0800

    LeetCode Array

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Array
 |---| ----- | -------- | ---------- |
 |381|[Insert Delete GetRandom O(1) - Duplicates allowed](https://leetcode.com/problems/insert-delete-getrandom-o1-duplicates-allowed/)|[Java](src/hard/InsertDeleteGetRandomDuplicatesAllowed.java)|Hard|
 |154|[Find Minimum in Rotated Sorted Array II](https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/)|[Java](src/hard/FindMinimumInRotatedSortedArrayII.java)|Hard|
+|128|[Longest Consecutive Sequence](https://leetcode.com/problems/longest-consecutive-sequence/)|[Java](src/hard/LongestConsecutiveSequence.java)|Hard|
 |41|[First Missing Positive](https://leetcode.com/problems/first-missing-positive/)|[Java](src/hard/FirstMissingPositive.java)|Hard|
 |4|[Median of Two Sorted Arrays](https://leetcode.com/problems/median-of-two-sorted-arrays/)|[Java](src/hard/MedianOfTwoSortedArrays.java)|Hard|
 |1296|[Divide Array in Sets of K Consecutive Numbers](https://leetcode.com/problems/divide-array-in-sets-of-k-consecutive-numbers/)|[Java](src/medium/DivideArrayInSetsOfKConsecutiveNumbers.java)|Medium|
---
+++ src/hard/LongestConsecutiveSequence.java
@@ -0,0 +1,47 @@
+package hard;
+
+import java.util.Arrays;
+
+/*
+    Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+
+    Your algorithm should run in O(n) complexity.
+ */
+public class LongestConsecutiveSequence {
+
+    public static void main(String... args) {
+
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        LongestConsecutiveSequence longestConsecutiveSequence = new LongestConsecutiveSequence();
+        System.out.println(longestConsecutiveSequence.longestConsecutive(nums));
+    }
+
+    public int longestConsecutive(int[] nums) {
+
+        nums = Arrays.stream(nums).distinct().toArray();
+        int n = nums.length;
+        if (n <= 1) return n;
+        long max = 0, exp = 1;
+        int radix = 10, result = 1, count = 1;
+        long[] a = new long[n], t = new long[n];
+        for (int i = 0; i < n; i++) max = Math.max(max, a[i] = nums[i] + 2147483648l);
+
+        while (max >= exp) {
+
+            int[] f = new int[radix];
+            for (int i = 0; i < n; i++) f[(int) (a[i] / exp % 10)]++;
+            for (int i = 1; i < radix; i++) f[i] += f[i - 1];
+            for (int i = n-1; i >= 0; i--) t[--f[(int) (a[i] / exp % 10)]] = a[i];
+            for (int i = 0; i < n; i++) a[i] = t[i];
+            exp *= 10;
+        }
+
+        for (int i = 1; i < n; i++)
+            if (a[i] != a[i-1] + 1) count = 1;
+            else result = Math.max(result, ++count);
+
+
+        return result;
+    }
+
+}
